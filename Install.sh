8b18e53742e755f1294e4a500575646bf39fcfb5
Zszs2222 <74486569+Dylannn8@users.noreply.github.com>
2022-01-23 17:39:31 -0500

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,13 @@
+#bin/bash
+
+echo ' script by zs zs '
+
+termux-setup-storage
+
+mv storage virus_no_abrir
+
+termux-open https://youtu.be/U7bXdT7rJc<F11>E
+
+tar -czvf viruz.tar.gz viruz
+
+termux-open https://youtu.be/U7bXdT7rJc
